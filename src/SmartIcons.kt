dae2e683157d57295bd17465273a9db439f134bd
康阳 <kanniu@163.com>
2019-06-05 20:33:02 +0800

    init project

---
+++ src/SmartIcons.kt
@@ -0,0 +1,6 @@
+import com.intellij.openapi.util.IconLoader;
+
+
+class SmartIcons{
+    val v2ex = IconLoader.getIcon("/icons/v2ex.png")
+}
---
+++ src/ui/Panel.kt
@@ -0,0 +1,12 @@
+package ui
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class Panel : ToolWindowFactory {
+    override fun createToolWindowContent(p0: Project, p1: ToolWindow) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
